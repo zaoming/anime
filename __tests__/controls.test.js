184e8df4a1bf0a57cf72b7ca90dfaeede7a8db11
Muhammad Ubaid Raza <mubaidr@gmail.com>
2019-01-18 15:54:00 +0500

    [WIP] Controls

--- __tests__/controls.test.js
+++ __tests__/controls.test.js
@@ -1,5 +1,161 @@
-describe('target', () => {
-  test('1=1', () => {
-    expect(1).toBe(1);
+describe('CONTROLS', () => {
+  // duration is used to reduce test run time
+
+  test('PLAY / PAUSE', done => {
+    const target = {opacity: 0};
+
+    const animation = anime({
+      targets: target,
+      opacity: 1,
+      duration: 100,
+      autoplay: false,
+      complete: () => {
+        expect(animation.progress).toBe(100);
+
+        done();
+      }
+    });
+
+    expect(animation.progress).toBe(0);
+    expect(animation.remaining).toBe(1);
+    expect(animation.began).toBeFalse();
+
+    animation.play();
+
+    window.setTimeout(() => {
+      animation.pause();
+
+      expect(animation.progress).toBeGreaterThan(0);
+      expect(animation.completed).toBeFalse();
+
+      animation.play();
+    }, 50);
+  });
+
+  test('RESTART', done => {
+    const target = {opacity: 0};
+
+    const animation = anime({
+      targets: target,
+      opacity: 1,
+      duration: 100,
+      autoplay: false
+    });
+
+    expect(animation.progress).toBe(0);
+    expect(animation.remaining).toBe(1);
+    expect(animation.began).toBeFalse();
+
+    animation.play();
+
+    window.setTimeout(() => {
+      animation.pause();
+
+      expect(animation.progress).toBeGreaterThan(0);
+      expect(animation.completed).toBeFalse();
+
+      animation.restart();
+
+      expect(animation.progress).toBe(0);
+      expect(animation.remaining).toBe(1);
+      expect(animation.began).toBeFalse();
+
+      done();
+    }, 100);
+  });
+
+  test('REVERSE', done => {
+    const target = {opacity: 0};
+
+    const animation = anime({
+      targets: target,
+      opacity: 1,
+      duration: 100,
+      autoplay: false,
+      loop: true
+    });
+
+    expect(animation.progress).toBe(0);
+    expect(animation.began).toBeFalse();
+
+    animation.play();
+
+    window.setTimeout(() => {
+      animation.pause();
+
+      expect(animation.progress).toBeGreaterThan(0);
+      expect(animation.completed).toBeFalse();
+
+      animation.restart();
+
+      expect(animation.progress).toBe(0);
+      expect(animation.began).toBeFalsy();
+
+      done();
+    }, 50);
+  });
+
+  test('SEEK', done => {
+    const target = {opacity: 0};
+
+    const animation = anime({
+      targets: target,
+      opacity: 1,
+      duration: 100,
+      autoplay: false
+    });
+
+    expect(animation.progress).toBe(0);
+    expect(animation.remaining).toBe(1);
+    expect(animation.began).toBeFalse();
+
+    animation.play();
+
+    window.setTimeout(() => {
+      animation.pause();
+
+      expect(animation.progress).toBeGreaterThan(0);
+      expect(animation.completed).toBeFalse();
+
+      animation.restart();
+
+      expect(animation.progress).toBe(0);
+      expect(animation.remaining).toBe(1);
+      expect(animation.began).toBeFalse();
+
+      done();
+    }, 100);
+  });
+
+  test('TIMELINE CONTROLS', done => {
+    const target = {opacity: 0};
+
+    const animation = anime({
+      targets: target,
+      opacity: 1,
+      duration: 100,
+      autoplay: false
+    });
+
+    expect(animation.progress).toBe(0);
+    expect(animation.remaining).toBe(1);
+    expect(animation.began).toBeFalse();
+
+    animation.play();
+
+    window.setTimeout(() => {
+      animation.pause();
+
+      expect(animation.progress).toBeGreaterThan(0);
+      expect(animation.completed).toBeFalse();
+
+      animation.restart();
+
+      expect(animation.progress).toBe(0);
+      expect(animation.remaining).toBe(1);
+      expect(animation.began).toBeFalse();
+
+      done();
+    }, 100);
   });
 });
