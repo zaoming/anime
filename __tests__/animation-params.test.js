40986b45764b3a65782f4a22e620b90aad9e0337
Muhammad Ubaid Raza <mubaidr@gmail.com>
2019-01-20 21:24:33 +0500

    WIP ANIMATION PARAMETERS

--- __tests__/animation-params.test.js
+++ __tests__/animation-params.test.js
@@ -1,5 +1,50 @@
-describe('target', () => {
-  test('1=1', () => {
-    expect(1).toBe(1);
+describe('ANIMATION PARAMETERS', () => {
+  test('DIRECTION', done => {
+    const target = {opacity: 0};
+
+    // normal
+    let animation = anime({
+      targets: target,
+      opacity: 100,
+      duration: 100,
+      easing: 'linear',
+      autoplay: false,
+      direction: 'normal'
+    });
+
+    animation.seek(40);
+    expect(target.opacity).toBeCloseTo(40);
+
+    // reverse
+    animation = anime({
+      targets: target,
+      opacity: 100,
+      duration: 100,
+      easing: 'linear',
+      autoplay: false,
+      direction: 'reverse'
+    });
+
+    animation.seek(40);
+    expect(target.opacity).toBeCloseTo(60);
+
+    // alternate
+    animation = anime({
+      targets: target,
+      opacity: 100,
+      duration: 100,
+      easing: 'linear',
+      // autoplay: false,
+      direction: 'alternate',
+      loop: true,
+      changeBegin: () => {
+        console.log(target);
+      },
+      changeComplete: () => {
+        console.log(target);
+      }
+    });
+
+    window.setTimeout(done, 1000);
   });
 });
