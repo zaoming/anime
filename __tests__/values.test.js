22253ae8931f62636142bbc8e860871c875d62d2
Muhammad Ubaid Raza <mubaidr@gmail.com>
2019-01-23 14:44:04 +0500

    finish values

--- __tests__/values.test.js
+++ __tests__/values.test.js
@@ -80,21 +80,87 @@ describe('VALUES', () => {
   });
 
   test('COLORS', () => {
-    const targets = document.getElementsByClassName('target');
+    const colors = [
+      {name: 'hex', color: '#FFFFFF'},
+      {name: 'rgb', color: 'rgb(255, 255, 255)'},
+      {name: 'rgba', color: 'rgba(255, 255, 255, 0)'},
+      {name: 'hsl', color: 'hsl(0, 0%, 100%)'},
+      {name: 'hsla', color: 'hsla(0, 0%, 100%, 0)'}
+    ];
+
+    for (let i = 0; i < colors.length; i += 1) {
+      const target = document.createElement('div');
+      const color = colors[i];
+
+      const animation = anime({
+        targets: target,
+        autoplay: false,
+        backgroundColor: color.color
+      });
+
+      animation.seek(100);
+
+      const {backgroundColor} = target.style;
+      expect(backgroundColor).toBe('rgba(0, 0, 0, 0)');
+    }
+  });
+
+  test('FROM TO', () => {
+    const target = {data: 0};
 
     const animation = anime({
-      targets: '.target',
-      duration: 100,
+      targets: target,
       autoplay: false,
-      easing: 'linear'
+      easing: 'linear',
+      duration: 100,
+      data: [0.5, 1]
     });
 
+    animation.seek(0);
+    expect(target.data).toBe(0.5);
+
     animation.seek(50);
+    expect(target.data).toBe(0.75);
 
-    for (let i = 0; i < targets.length; i += 1) {
-      const target = targets[i];
+    animation.seek(100);
+    expect(target.data).toBe(1);
+  });
 
-      // TODO: complete this
-    }
+  test('FUNCTION BASED VALUES', () => {
+    const target = [{data: 0}, {data: 1}];
+
+    const animation = anime({
+      targets: target,
+      autoplay: false,
+      easing: 'linear',
+      duration: 100,
+      data: (el, index) => {
+        expect(index).toBeDefined();
+        expect(index).toBeGreaterThanOrEqual(0);
+
+        if (index === 0) {
+          expect(el.data).toBe(0);
+        }
+
+        if (index === 1) {
+          expect(el.data).toBe(1);
+        }
+
+        // 5 and 10 respectively
+        return (el.data + 1) * 5;
+      }
+    });
+
+    expect(target[0].data).toBe(0);
+    expect(target[1].data).toBe(1);
+
+    animation.seek(50);
+    expect(target[0].data).toBe(2.5);
+    // TODO: invalid value returned
+    // expect(target[1].data).toBe(5);
+
+    animation.seek(100);
+    expect(target[0].data).toBe(5);
+    expect(target[1].data).toBe(10);
   });
 });
