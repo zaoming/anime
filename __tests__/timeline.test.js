118a2de40a6bd6c0163a463b7b2768a7a4848f16
Muhammad Ubaid Raza <mubaidr@gmail.com>
2019-01-23 15:52:29 +0500

    [WIP] timeline

--- __tests__/timeline.test.js
+++ __tests__/timeline.test.js
@@ -1,5 +1,75 @@
-describe('target', () => {
-  test('1=1', () => {
-    expect(1).toBe(1);
+describe('TIMELINE', () => {
+  test('TIMELINE BASICS', () => {
+    const target = [{data: 0}, {data: 0}, {data: 0}];
+
+    const timeline = anime.timeline({
+      autoplay: false,
+      easing: 'linear',
+      duration: 100
+    });
+
+    target.forEach(t => {
+      timeline.add({
+        targets: t,
+        data: 10
+      });
+    });
+  });
+
+  test('TIME OFFSETS', () => {
+    // asolute offset
+    let target = [{data: 0}, {data: 0}, {data: 0}];
+    let timeline = anime.timeline({
+      autoplay: false,
+      easing: 'linear',
+      duration: 100
+    });
+
+    target.forEach((t, index) => {
+      timeline.add(
+        {
+          targets: t,
+          data: 10
+        },
+        index * 2
+      );
+    });
+
+    // relative offset
+    target = [{data: 0}, {data: 0}, {data: 0}];
+    timeline = anime.timeline({
+      autoplay: false,
+      easing: 'linear',
+      duration: 100
+    });
+
+    target.forEach((t, index) => {
+      timeline.add(
+        {
+          targets: t,
+          data: 10
+        },
+        `+=${index * 2}`
+      );
+    });
+  });
+
+  test('PARAMETERS INHERITANCE', () => {
+    let target = [{data: 0}, {data: 0}, {data: 0}];
+    let timeline = anime.timeline({
+      autoplay: false,
+      easing: 'linear',
+      duration: 100
+    });
+
+    target.forEach((t, index) => {
+      timeline.add(
+        {
+          targets: t,
+          data: 10
+        },
+        index * 2
+      );
+    });
   });
 });
