afa2df1c870067b7f7ec1d3d7eeccc8eee8a7784
Muhammad Ubaid Raza <mubaidr@gmail.com>
2019-01-28 15:55:29 +0500

    [WIP] SVG

--- __tests__/__setup_framework.js
+++ __tests__/__setup_framework.js
@@ -1,9 +1,6 @@
 require('jest-dom/extend-expect');
 require('jest-extended');
 
-// configure anime globals
-// global.anime = require('../src');
-
 // prepare template for testing
 const targetA = document.createElement('div');
 const targetB = document.createElement('div');
@@ -18,6 +15,15 @@ document.body.appendChild(targetA);
 document.body.appendChild(targetB);
 document.body.appendChild(targetC);
 document.body.appendChild(targetD);
+
+// adds an SVG document with path
+var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+const p = document.createElementNS(svg.namespaceURI, 'path');
+p.setAttribute('d', 'M0 0 L100 100');
+svg.appendChild(p);
+svg.id = 'target-svg';
+
+document.body.appendChild(svg);
 
 // resets style between each test
 beforeEach(() => {
--- __tests__/svg.test.js
+++ __tests__/svg.test.js
@@ -1,5 +1,26 @@
-describe('target', () => {
-  test('1=1', () => {
-    expect(1).toBe(1);
+describe('SVG', () => {
+  test('MOTION PATH', () => {
+    const target = {
+      x: 0,
+      y: 0,
+      angle: 0
+    };
+
+    var path = anime.path('#target-svg');
+
+    const animation = anime({
+      targets: target,
+      translateX: path('x'),
+      translateY: path('y'),
+      rotate: path('angle'),
+      easing: 'linear',
+      duration: 100,
+      autoplay: false,
+      loop: false
+    });
+
+    animation.seek(500);
+
+    console.log(target);
   });
 });
