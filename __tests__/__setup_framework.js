d9c0d8a7898331e71eb8812a213367b22e16998a
Muhammad Ubaid Raza <mubaidr@gmail.com>
2019-01-31 12:22:44 +0500

    update template for testing

--- __tests__/__setup_framework.js
+++ __tests__/__setup_framework.js
@@ -1,38 +1,21 @@
-require('jest-dom/extend-expect');
 require('jest-extended');
 
-// prepare template for testing
-const targetA = document.createElement('div');
-const targetB = document.createElement('div');
-const targetC = document.createElement('div');
-const targetD = document.createElement('div');
+const anime = require('../src');
 
-targetA.className = 'target';
-targetB.className = 'target';
-targetC.id = 'target';
-
-document.body.appendChild(targetA);
-document.body.appendChild(targetB);
-document.body.appendChild(targetC);
-document.body.appendChild(targetD);
-
-// adds an SVG document with path
-var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-const p = document.createElementNS(svg.namespaceURI, 'path');
-p.setAttribute('d', 'M0 0 L100 100');
-svg.appendChild(p);
-svg.id = 'target-svg';
-
-document.body.appendChild(svg);
-
-// resets style between each test
 beforeEach(() => {
-  const targets = [...document.getElementsByTagName('div')];
+  // prepare template for testing
+  document.body.innerHTML = `
+    <!-- DIV targets -->
+    <div class='target'></div>
+    <div class='target'></div>
+    <div id='target'></div>
 
-  targets.forEach(target => {
-    target.removeAttribute('style');
-  });
+    <!-- SVG -->
+    <svg id='target-svg' width="128" height="128" viewBox="0 0 128 128">
+      <path id="target-svg-path" d="M0 0 L100 100" />
+    </svg>
+  `;
 
-  // configure anime globals
-  global.anime = require('../src');
+  // reset anime global
+  global.anime = anime;
 });
